b0dc12e46246a8c1179389229dc22687e1d8e88f
nancynarmadhathomas <aachinancy@gmail.com>
2026-07-15 14:26:38 +0530

    fix: force google oauth account chooser panel to prevent automatic redirects

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       authorization: {
         params: {
           redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+          prompt: "select_account",
+          access_type: "offline",
+          response_type: "code",
         },
       },
     }),
